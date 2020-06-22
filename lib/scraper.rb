2988630c8a6432643587db60e6eee6fe5115b8d2
Wenhao Guo <gwh1742072653@gmail.com>
2020-06-22 19:02:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
     student_hash = {}
-    doc.css(".)
+    doc.css(".social-icon-container)
     
     
   end
