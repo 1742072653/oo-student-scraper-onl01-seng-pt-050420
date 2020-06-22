0b4b68a50227f618568aa964f37b2a44e17f069e
Wenhao Guo <gwh1742072653@gmail.com>
2020-06-22 19:33:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,11 +16,11 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each {|key,value| self.send("#{key}=",value)}
   end
 
   def self.all
-    
+    @all
   end
 end
 
