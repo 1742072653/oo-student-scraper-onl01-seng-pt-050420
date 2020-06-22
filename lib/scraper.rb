d20a6e596ccc276bdce504c264797a18a5c668bd
Wenhao Guo <gwh1742072653@gmail.com>
2020-06-22 18:36:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,7 +11,7 @@ class Scraper
       new_hash = {
         :name => student.css("h4").text,
         :location => student.css("p").text,
-        :profile_url => student.css("a")["href"]
+        :profile_url => student.css("a").attribute("href").value
       }
       student_arr << new_hash
     end
