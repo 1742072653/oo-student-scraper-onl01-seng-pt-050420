8625d322789391306e9a062ad126160c1de72314
Wenhao Guo <gwh1742072653@gmail.com>
2020-06-22 18:15:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,12 +6,14 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/"))
     student_arr = []
+    
     doc.css(".student-card").each do |student|
       new_hash = {
         :name => student.css(".student-name").text
         :location => student.css(".student-location").text 
         :profile_url => student.css("a")['href']
       }
+      end
       student_arr << new_hash
     end
     student_arr
